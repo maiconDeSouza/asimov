9743b28eef0ca4cecebd79b6b6caed5f4cdc104d
Maicon Souza <maicon_souza09@hotmail.com>
2024-07-14 20:26:51 -0300

    14-07-24 20:26

--- master_class/booleanos.py
+++ master_class/booleanos.py
@@ -1,1 +1,3 @@
 is_valid = True
+
+print(3 > 2 and is_valid)
---
+++ master_class/controle_de_fluxo.py
@@ -0,0 +1,7 @@
+idade = 18
+teste_psicotecnico = False
+
+if idade >= 18 and teste_psicotecnico:
+    print("Pode Tirar a Carteira")
+else:
+    print("Não Pode Tirar a Carteira")
